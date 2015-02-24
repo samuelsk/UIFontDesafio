c79357a31bcf3632b48d041b20ac40de99e4dd3a
Samuel <Samuel@Samuels-Macbook-Pro.local>
2015-02-24 17:40:52 -0300

    Desafio completo

--- UIFontDesafio/ViewController.h
+++ UIFontDesafio/ViewController.h
@@ -8,8 +8,17 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController {
+    __weak IBOutlet UITextField *nomeInput;
+    __weak IBOutlet UILabel *nomeLabel;
+}
 
+
+- (IBAction)alterarNome:(id)sender;
+- (IBAction)fontHelvetica:(id)sender;
+- (IBAction)fontZapfino:(id)sender;
+- (IBAction)fontPapyrus:(id)sender;
+- (IBAction)fontCourierNew:(id)sender;
 
 @end
 
--- UIFontDesafio/ViewController.m
+++ UIFontDesafio/ViewController.m
@@ -19,9 +19,32 @@
     
 }
 
+- (void) touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    [self.view endEditing:YES];
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)alterarNome:(id)sender {
+    nomeLabel.text = nomeInput.text;
+}
+
+- (IBAction)fontHelvetica:(id)sender {
+    nomeLabel.font = [UIFont fontWithName:@"Helvetica" size:20];
+}
+
+- (IBAction)fontZapfino:(id)sender {
+    nomeLabel.font = [UIFont fontWithName:@"Zapfino" size:20];
+}
+
+- (IBAction)fontPapyrus:(id)sender {
+    nomeLabel.font = [UIFont fontWithName:@"Papyrus" size:20];
+}
+
+- (IBAction)fontCourierNew:(id)sender {
+    nomeLabel.font = [UIFont fontWithName:@"CourierNewPSMT" size:20];
+}
 @end
